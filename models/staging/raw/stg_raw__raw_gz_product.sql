c165f132a3eff77d17f05588314bd52a5671115c
Fatih YARDIM <fatihyardim81@gmail.com>
2025-12-14 19:52:15 +0000

    initial commit

---
+++ models/staging/raw/stg_raw__raw_gz_product.sql
@@ -0,0 +1,19 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'raw_gz_product') }}
+
+),
+
+renamed as (
+
+    select
+        products_id,
+        SAFE_CAST(purchse_price AS FLOAT64) AS purchase_price --purchse_price
+
+    from source
+
+)
+
+select * from renamed
---
+++ models/staging/raw/stg_raw__raw_gz_ship.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'raw_gz_ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        --shipping_fee,
+        --shipping_fee_1,
+        shipping_fee
+        WHERE shipping_fee IS DISTINCT FROM shipping_fee_1,
+        logcost,
+        SAFE_CAST(ship_cost AS FLOAT64) AS ship_cost --ship_cost
+
+    from source
+
+)
+
+select * from renamed
